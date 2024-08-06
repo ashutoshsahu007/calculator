2ea8cea33c30592442214b8347a03bfa600bccb4
ashutosh sahu <ppaskumar@gmail.com>
2024-08-06 12:27:04 +0530

    structure of app completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./App.module.css";
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+
 const App = () => {
-  return <h1>calculator</h1>;
+  return (
+    <div className={styles.calculator}>
+      <Display />
+      <ButtonsContainer />
+    </div>
+  );
 };
 export default App;
